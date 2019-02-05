f70873ee6ec668f4401960081fd93904627c7433
Tominiyi Ajayi <tominiyimunito@tominiyis-air.home>
2019-02-05 01:07:19 +0000

    Done

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,50 @@
 class Artist
 
-  attr_reader :name, :years_experience
+  attr_accessor :name, :years_experience
+
+  @@all = []
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, gallery, self)
+  end
+
+
+  def paintings
+    Painting.all.select {|ele| ele.artist == self}
+  end
+
+  def galleries
+    paintings.map {|ele| ele.gallery}
+  end
+
+  def cities
+    galleries.map {|ele| ele.city}
+  end
+
+  def self.total_experience
+    a = @@all.map {|ele| ele.years_experience}
+    a.inject(:+)
+  end
+
+  def paintings_per_year
+    paintings.count.to_f / @years_experience.to_f
+  end
+
+  def most_prolific
+    @@all.max_by {|x| x.paintings_per_year}
+  end
+
+
+
 
 end
